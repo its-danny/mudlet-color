056954c7c58baf1990da5f734c3bd3977656823e
Danny Tatom <dannytatom@gmail.com>
2020-06-14 13:36:50 -0700

    refactor: fix typescript

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,56 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
-const yaml = require("js-yaml");
-const xml2js = require("xml2js");
-const yargs = require("yargs");
+import { green, red } from "chalk";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { safeLoad } from "js-yaml";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Builder, Parser } from "xml2js";
+import yargs from "yargs";
 
-const { argv } = yargs.usage("Usage: $0 -p [str] -t [str]").options({
-  p: {
-    alias: "path",
-    type: "string",
-    describe: "Path to profile",
-  },
-  t: {
-    alias: "theme",
-    type: "string",
-    describe: "Name of theme",
-  },
-  s: {
-    alias: "themes",
-    type: "boolean",
-    describe: "Get a list of valid themes",
-  },
-});
+// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const { argv } = yargs
+  .usage("Usage: $0 -p [str] -t [str]")
+  .options({
+    p: {
+      alias: "path",
+      type: "string",
+      describe: "Path to profile",
+      demandOption: true,
+    },
+    t: {
+      alias: "theme",
+      type: "string",
+      describe: "Name of theme",
+      demandOption: true,
+    },
+  })
+  .conflicts("s", "p")
+  .conflicts("s", "t");
 
 const themePath = `${path.join(__dirname, "..")}/themes/${argv.t}.yml`;
 const profilePath = argv.p;
 
-const getThemeList = () => {
-  console.log("https://mayccoll.github.io/Gogh/\n");
-
-  console.log(
-    fs
-      .readdirSync(`${path.join(__dirname, "..")}/themes`)
-      .map((theme) => theme.replace(".yml", ""))
-      .join(", "),
-  );
-
-  process.exit();
-};
-
 const checkPaths = () => {
-  if (!fs.existsSync(themePath)) {
-    console.log("Not a valid theme!");
+  if (!existsSync(themePath)) {
+    console.log(red("Not a valid theme!"));
     process.exit();
   }
 
-  if (!fs.existsSync(profilePath)) {
-    console.log("Can't find profile!");
+  if (!existsSync(profilePath)) {
+    console.log(red("Can't find profile!"));
     process.exit();
   }
 };
 
 const writeProfile = async () => {
-  const parser = new xml2js.Parser();
-  const builder = new xml2js.Builder();
+  const parser = new Parser();
+  const builder = new Builder();
 
-  const theme = yaml.safeLoad(fs.readFileSync(themePath, "utf8"));
-  const profile = fs.readFileSync(profilePath);
+  const theme = safeLoad(readFileSync(themePath, "utf8"));
+  const profile = readFileSync(profilePath);
 
   try {
     const result = await parser.parseStringPromise(profile);
@@ -66,17 +59,13 @@ const writeProfile = async () => {
       result.MudletPackage.HostPackage[0].Host[0][key] = theme[key];
     });
 
-    fs.writeFileSync(profilePath, builder.buildObject(result));
-    console.log("Profile updated!");
+    writeFileSync(profilePath, builder.buildObject(result));
+    console.log(green("Profile updated!"));
   } catch (err) {
-    console.log("Can't parse profile!");
+    console.log(red("Can't parse profile!"));
     process.exit();
   }
 };
-
-if (argv.s) {
-  getThemeList();
-}
 
 if (argv.p && argv.t) {
   checkPaths();
